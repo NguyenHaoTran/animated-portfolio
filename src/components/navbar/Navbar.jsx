0b22f46fa9ad86050b5e4b41e8f5e2bb14e4bb45
ng.Hao.t <ng.hao.t@ngHaots-Mac-mini.local>
2024-06-02 22:34:32 +0700

    update_code_navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+import "./navbar.scss";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <div className="wrapper">
+        <motion.span
+          initial={{ opacity: 0, scale: 0.5, x:100 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          Ng.hao.T
+        </motion.span>
+        <div className="social">
+          <a href="#">
+            <img src="/facebook.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/instagram.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/youtube.png" alt="" />
+          </a>
+          <a href="#">
+            <img src="/dribbble.png" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
